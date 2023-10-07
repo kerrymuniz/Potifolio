30550173cd323d5914ad0bbbe3ab1da5a7a57c2c
Kerry Muniz <kerrym877@gmail.com>
2023-10-07 18:02:54 -0300

    continuacao do portifolio

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ $(document).ready(function() { //o $(document).ready() serve para quando quiser 
             $('.navbar').removeClass("sticky");
         }
     });
+    $('.menu-btn').click(function(){
+        $('.navbar.menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    
     $('.carousel').owlCarousel({ //essa função dá um efeito de "arrasta pro lado" que fica bem bonito e estiloso;
-
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPouser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
     });
 });
