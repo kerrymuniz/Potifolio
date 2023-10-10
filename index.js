a564d7eecbc1628b3b9272a55e082fb41694129a
Kerry Muniz <kerrym877@gmail.com>
2023-10-09 23:28:36 -0300

    finalizacao do video do portifolio

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ $(document).ready(function() { //o $(document).ready() serve para quando quiser 
             $('.navbar').addClass("sticky"); //estou recuperando a classe  .navbar e adicionando a classe stick (.navbar .sticky{});
         } else {
             $('.navbar').removeClass("sticky");
+        } if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+        $('.scroll-up-btn').click(function () {
+            $('html').animate({scrollTop: 0});
+        });
+
+        var typed = new Typed(".typing", { /*..essas instruções vão dar um efeito de escrever automaticamente na tela (efeito typing...)..*/
+            strings:["Programador","Desenvolvedor","Web Developer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+
+        var typed = new Typed(".typing-2", { /*..essas instruções vão dar um efeito de escrever automaticamente na tela (efeito typing...)..*/
+            strings:["Programador","Desenvolvedor","Web Developer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+
+
     $('.menu-btn').click(function(){
         $('.navbar.menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
