8bb9bce2b1e02775388024a77a72a55518f3424e
Kerry Muniz <kerrym877@gmail.com>
2023-11-13 21:08:41 -0300

    voltando comitacao

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-document.getElementById(document).ready(function() { //o $(document).ready() serve para quando quiser carregar o conteúdo antes da página carregar;
-    document.getElementById(window).scroll(function() { //método usado para rolar a janela para um local específico;
+$(document).ready(function() { //o $(document).ready() serve para quando quiser carregar o conteúdo antes da página carregar;
+    $(window).scroll(function() { //método usado para rolar a janela para um local específico;
         if(this.scrollY > 20) {
-            document.getElementById('.navbar').addClass("sticky"); //estou recuperando a classe  .navbar e adicionando a classe stick (.navbar .sticky{});
+            $('.navbar').addClass("sticky"); //estou recuperando a classe  .navbar e adicionando a classe stick (.navbar .sticky{});
         } else {
-            document.getElementById('.navbar').removeClass("sticky");
+            $('.navbar').removeClass("sticky");
         } if(this.scrollY > 500) {
-            document.getElementById('.scroll-up-btn').addClass("show");
+            $('.scroll-up-btn').addClass("show");
         } else {
-            document.getElementById('.scroll-up-btn').removeClass("show");
+            $('.scroll-up-btn').removeClass("show");
         }
     });
-    document.getElementById('.scroll-up-btn').click(function () {
-        document.getElementById('html').animate({scrollTop: 0});
+        $('.scroll-up-btn').click(function () {
+            $('html').animate({scrollTop: 0});
         });
 
         var typed = new Typed(".typing", { /*..essas instruções vão dar um efeito de escrever automaticamente na tela (efeito typing...)..*/
@@ -29,12 +29,12 @@ document.getElementById(document).ready(function() { //o $(document).ready() ser
         });
 
 
-        document.getElementById('.menu-btn').click(function(){
-            document.getElementById('.navbar.menu').toggleClass("active");
-            document.getElementById('.menu-btn i').toggleClass("active");
+    $('.menu-btn').click(function(){
+        $('.navbar.menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
     
-    document.getElementById('.carousel').owlCarousel({ //essa função dá um efeito de "arrasta pro lado" que fica bem bonito e estiloso;
+    $('.carousel').owlCarousel({ //essa função dá um efeito de "arrasta pro lado" que fica bem bonito e estiloso;
         margin:20,
         loop:true,
         autoplayTimeOut:2000,
